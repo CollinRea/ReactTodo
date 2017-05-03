117c1d75d6a13f18455df030e984acfbb68a6af1
Collin Rea <collinr01@gmail.com>
2017-05-02 21:28:51 -0400

    AddTodo component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+  handleSubmit: function (e) {
+    e.preventDefault();
+    var todo = this.refs.todo.value;
+    
+    if (todo.length > 0) {
+      this.props.onAddTodo(todo);
+    } else {
+      this.refs.todo.focus()
+    }
+    this.refs.todo.value = '';
+  },
+  render: function () {
+    return (
+      <div>
+        <form ref="form" onSubmit={this.handleSubmit}>
+          <input type="text" ref="todo" placholder="Enter Todo"/>
+          <button className="button expanded">Add</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+module.exports = AddTodo;
