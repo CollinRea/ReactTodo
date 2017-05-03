b1976698a628cd354934beaeaf6bb6335e356263
Collin Rea <collinr01@gmail.com>
2017-05-03 06:18:11 -0400

    Wire up handleAddTodo in TodoApp and add test

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -9,5 +9,15 @@ var TodoApp = require('TodoApp');
 describe('TodoApp', () => {
   it('should exist', () => {
     expect(TodoApp).toExist();
-  })
+  });
+
+  it('should add new todo item', () => {
+    var todoText = 'Test Todo';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>)
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+    
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
 });
