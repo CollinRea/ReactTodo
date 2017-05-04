040559251854f523d8dd0096a3df0b85ed8533f4
Collin Rea <collinr01@gmail.com>
2017-05-04 06:07:35 -0400

    Setup TodoAPI to store todos in local storage, can expand this to use an external database later

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,35 +4,18 @@ var uuid = require('node-uuid');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
     return {
       showCompleted: false,
       searchText: '',
-      todos: [
-        {
-          id: uuid(),
-          text: 'Walk the dog',
-          completed: true
-        },
-        {
-          id: uuid(),
-          text: 'Rake the yard',
-          completed: false
-        },
-        {
-          id: uuid(),
-          text: 'Go for a Run',
-          completed: true
-        },
-        {
-          id: uuid(),
-          text: 'Take out the Trash',
-          completed: false
-        },
-      ]
+      todos: TodoAPI.getTodos()
     };
+  },
+  componentDidUpdate: function () {
+    TodoAPI.setTodos(this.state.todos);
   },
   handleToggle: function (id) {
     var updatedTodos = this.state.todos.map((todo) => {
