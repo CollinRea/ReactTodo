ec3e583e1a9fcaf9ae7c725bc72283b6f1577ff8
Collin Rea <collinr01@gmail.com>
2017-05-06 15:48:47 -0400

    Style todo application

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -25,4 +25,13 @@ describe('TodoList', () => {
 
     expect(todosComponents.length).toBe(todos.length);
   });
+
+  it('should render an empty message with no Todos', () => {
+    var todos = [];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var $el = $(ReactDOM.findDOMNode(todoList));
+
+    expect($el.find('.container__message').length).toBe(1);
+  });
+  
 });
