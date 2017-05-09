3572a39dad59e5342100ea28a41dff27daacd864
Collin Rea <collinr01@gmail.com>
2017-05-09 19:49:07 -0400

    Complete all actions and tests

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -11,4 +11,33 @@ describe('Actions', () => {
 
     expect(res).toEqual(action);
   });
+
+  it('should generate add todo actions', () => {
+    var action = {
+      type: 'ADD_TODO',
+      text: 'Some todo'
+    };
+    var res = actions.addTodo(action.text);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggle show completed action', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+    var res = actions.toggleShowCompleted();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggle completed action', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: 1
+    };
+    var res = actions.toggleTodo(action.id);
+
+    expect(res).toEqual(action);
+  });
 });
