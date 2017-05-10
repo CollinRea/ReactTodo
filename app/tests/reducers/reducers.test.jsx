4951281ebab03dcec0b0d0d49369d1b0878971a1
Collin Rea <collinr01@gmail.com>
2017-05-10 19:53:49 -0400

    Todo reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -15,6 +15,7 @@ describe('Reducers', () => {
       expect(res).toEqual(action.searchText);
     });
   });
+
   describe('showCompletedReducer', () => {
     it('should toggle showCompleted', () => {
       var action = {
@@ -23,6 +24,39 @@ describe('Reducers', () => {
       var res = reducers.showCompletedReducer(df(false), df(action));
 
       expect(res).toEqual(true);
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Test Todo'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle completed status', () => {
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+      var state = [
+        {
+          id: 1,
+          text: 'Test todo',
+          completed: false,
+          createdAt: '01/01/2017',
+          completedAt: undefined
+        }
+      ];
+      var res = reducers.todosReducer(df(state), df(action));
+      
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toNotBe(undefined);
     });
   });
 });
