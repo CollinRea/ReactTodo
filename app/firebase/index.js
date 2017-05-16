02bb526c7337dc78cc350f60a64f08964bf2abc2
Collin Rea <collinr01@gmail.com>
2017-05-16 05:46:49 -0400

    Create test and development database and put api keys in ENV variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,12 +3,10 @@ import firebase from 'firebase';
 // Initialize Firebase in Try/Catch to limit initialization to once
 try {
   var config = {
-    apiKey: "AIzaSyDN4VseQ-l1j_WphEDKB2xLnnYrr2HB9Zg",
-    authDomain: "my-todo-app-f0c3c.firebaseapp.com",
-    databaseURL: "https://my-todo-app-f0c3c.firebaseio.com",
-    projectId: "my-todo-app-f0c3c",
-    storageBucket: "my-todo-app-f0c3c.appspot.com",
-    messagingSenderId: "103493223913"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET
   };
   firebase.initializeApp(config);
 } catch (e) {
