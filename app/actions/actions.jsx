46c2f697e9b8fda08d756d46a62d145673af1aeb
Collin Rea <collinr01@gmail.com>
2017-05-17 06:15:47 -0400

    Add github auth to app and linked to login and logout buttons still WIP

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,4 +1,4 @@
-import firebase, {firebaseRef} from 'app/firebase/';
+import firebase, {firebaseRef, githubProvider} from 'app/firebase/';
 import moment from 'moment';
 
 export var setSearchText = (searchText) => {
@@ -86,6 +86,26 @@ export var startToggleTodo = (id, completed) => {
     };
     return todoRef.update(updates).then(() => {
       dispatch(updateTodo(id, updates));
+    });
+  };
+};
+
+export var startLogin = () => {
+  return (dispatch, getState) => {
+    return firebase.auth().signInWithPopup(githubProvider).then((result) => {
+      console.log('Auth Worked', result);
+    }, (e) => {
+      console.log('Unable to auth', e);
+    });
+  };
+};
+
+export var startLogout = () => {
+  return (dispatch, getState) => {
+    return firebase.auth().signOut().then((success) => {
+      console.log('Logged Out');
+    }, (e) => {
+      console.log('Unable to log out', e);
     });
   };
 };
--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,6 +1,13 @@
 var React = require('react');
+import * as Redux from 'react-redux';
+
+import * as actions from 'actions';
 
 export var Login = React.createClass({
+  onLogin() {
+    var {dispatch} = this.props;
+    dispatch(actions.startLogin());
+  },
   render() {
     return (
       <div>
@@ -10,7 +17,7 @@ export var Login = React.createClass({
             <div className="callout callout-auth">
               <h3>Login</h3>
               <p>Login with GitHub account below.</p>
-              <button className="button">Login With Github</button>
+              <button className="button" onClick={this.onLogin}>Login With Github</button>
             </div>
           </div>
         </div>
@@ -19,4 +26,4 @@ export var Login = React.createClass({
   }
 });
 
-export default Login;
+export default Redux.connect()(Login);
