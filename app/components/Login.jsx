cb0398a02d14f96ff1592f6d2cb0ab654475dad9
Collin Rea <collinr01@gmail.com>
2017-05-25 19:45:35 -0400

    Convert all components to es6 classes

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -4,11 +4,15 @@ import * as Redux from 'react-redux';
 import * as actions from 'actions';
 import firebase from 'app/firebase/';
 
-export var Login = React.createClass({
+export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onLogin = this.onLogin.bind(this);
+  }
   onLogin() {
     var {dispatch} = this.props;
     dispatch(actions.startLogin());
-  },
+  }
   render() {
     return (
       <div>
@@ -25,6 +29,6 @@ export var Login = React.createClass({
       </div>
     );
   }
-});
+}
 
 export default Redux.connect()(Login);
